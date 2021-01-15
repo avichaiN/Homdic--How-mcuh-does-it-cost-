88903ab70b27a3e6389d96b08168ccd9547d4ed1
Avichai Nissim <avichai74@gmail.com>
2021-01-15 16:19:34 +0200

    1

--- Homid/public/js/dist/choseCategory.dev.js
+++ Homid/public/js/dist/choseCategory.dev.js
@@ -1,14 +1,10 @@
 "use strict";
 
-var showUserInfo = function showUserInfo(ev) {
-  document.querySelector('.cateogryPage__headerUserInfoShow').style.display = 'none';
-  document.querySelector('.cateogryPage__headerUserInfoDrop').style.opacity = '1';
-  document.querySelector('.cateogryPage__headerUserInfoDrop').style.height = '89px';
-  ev.stopPropagation();
+var showUserDropDown = function showUserDropDown(e) {
+  document.querySelector('.cateogryPage__headerUserInfoDrop').style.display = 'flex';
+  e.stopPropagation();
 };
 
-var hideUserInfo = function hideUserInfo() {
-  document.querySelector('.cateogryPage__headerUserInfoShow').style.display = 'block';
-  document.querySelector('.cateogryPage__headerUserInfoDrop').style.opacity = '0';
-  document.querySelector('.cateogryPage__headerUserInfoDrop').style.height = '0';
+var hideUserDropDown = function hideUserDropDown() {
+  document.querySelector('.cateogryPage__headerUserInfoDrop').style.display = 'none';
 };
