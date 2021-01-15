8740e58f44f0c6420edc44296ab013d094d97cdb
Avichai Nissim <avichai74@gmail.com>
2021-01-15 16:00:29 +0200

    1

--- Homid/public/js/choseCategory.js
+++ Homid/public/js/choseCategory.js
@@ -1,8 +1,11 @@
-const showUserInfo = () => {
+const showUserInfo = (ev) => {
     document.querySelector('.cateogryPage__headerUserInfoShow').style.display = 'none'
-    document.querySelector('.cateogryPage__headerUserInfoDrop').style.display = 'flex'
+    document.querySelector('.cateogryPage__headerUserInfoDrop').style.opacity = '1'
+    document.querySelector('.cateogryPage__headerUserInfoDrop').style.height = '89px'
+    ev.stopPropagation();
 }
 const hideUserInfo = () => {
     document.querySelector('.cateogryPage__headerUserInfoShow').style.display = 'block'
-    document.querySelector('.cateogryPage__headerUserInfoDrop').style.display = 'none'
+    document.querySelector('.cateogryPage__headerUserInfoDrop').style.opacity = '0'
+    document.querySelector('.cateogryPage__headerUserInfoDrop').style.height = '0'
 }
--- Homid/public/js/dist/choseCategory.dev.js
+++ Homid/public/js/dist/choseCategory.dev.js
@@ -1,11 +1,14 @@
 "use strict";
 
-var showUserInfo = function showUserInfo() {
+var showUserInfo = function showUserInfo(ev) {
   document.querySelector('.cateogryPage__headerUserInfoShow').style.display = 'none';
-  document.querySelector('.cateogryPage__headerUserInfoDrop').style.display = 'flex';
+  document.querySelector('.cateogryPage__headerUserInfoDrop').style.opacity = '1';
+  document.querySelector('.cateogryPage__headerUserInfoDrop').style.height = '89px';
+  ev.stopPropagation();
 };
 
 var hideUserInfo = function hideUserInfo() {
   document.querySelector('.cateogryPage__headerUserInfoShow').style.display = 'block';
-  document.querySelector('.cateogryPage__headerUserInfoDrop').style.display = 'none';
+  document.querySelector('.cateogryPage__headerUserInfoDrop').style.opacity = '0';
+  document.querySelector('.cateogryPage__headerUserInfoDrop').style.height = '0';
 };
