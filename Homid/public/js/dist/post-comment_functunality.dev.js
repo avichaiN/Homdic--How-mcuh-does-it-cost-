62b662bd6b5abad48df637b96389ea95ce95afe1
eyal home <eshemuel1@gmail.com>
2021-01-17 22:15:36 +0200

    fixt posts

---
+++ Homid/public/js/dist/post-comment_functunality.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function showAddpost() {
+  document.querySelector(".AddPost").classList.replace("hide", "show");
+  document.querySelector("#addPostbutton").classList.replace("show", "hide");
+  document.querySelector("#removePostbutton").classList.replace("hide", "show");
+}
+
+function hideAddpost() {
+  console.log("hide");
+  document.querySelector(".AddPost").classList.replace("show", "hide");
+  document.querySelector("#addPostbutton").classList.replace("hide", "show");
+  document.querySelector("#removePostbutton").classList.replace("show", "hide");
+}
