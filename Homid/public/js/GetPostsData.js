968f39cd5215ba8545efabe46b80ec57d926fba9
eyal shemuel <eshemuel1@gmail.com>
2021-01-21 17:43:24 +0200

    1

--- Homid/public/js/GetPostsData.js
+++ Homid/public/js/GetPostsData.js
@@ -1,16 +1,3 @@
-// get post data when for category
-// const getDisplayCategories = () => {
-//   fetch("/posts/category/get")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       if (data.status === "unauthorized") {
-//         window.location.replace("index.html");
-//       } else {
-//         let categories = data.categories;
-//         writeCategoiresToDom(categories);
-//       }
-//     });
-// };
 
 const getPostsByIdInParams = () =>{
   const url = window.location.href
