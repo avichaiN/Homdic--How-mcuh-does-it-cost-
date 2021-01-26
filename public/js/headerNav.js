8e2554be8e60c225674ab1e7b91e3ac556a8982b
Morde Yoiris <morde1515@gmail.com>
2021-01-26 18:33:50 +0200

    Morde add

--- public/js/headerNav.js
+++ public/js/headerNav.js
@@ -1,91 +1,82 @@
 const renderNavToDom = () => {
-    let html = ''
-    html = `<div class="header__helloUser"></div>
-    <a href="/adminPage.html" class="header__adminPage">דף אדמין</a>
-    <form class="header__form" onsubmit="handleSearch(event)">
-        <input class="header__formInput" placeholder='חפש פוסטים' type="text" required>
-        <!-- <input class="header__formSubmit" type="submit" value="חפש"> -->
-        <button class="header__formSubmit" type="submit"><i class="fa fa-search"></i></button>
-    </form>
+  let html = "";
+  html = `<div class="header__helloUser"></div>
+      <form class="header__form" onsubmit="handleSearch(event)">
+          <input class="header__formInput" placeholder='חפש פוסטים' type="text" required>
+          <!-- <input class="header__formSubmit" type="submit" value="חפש"> -->
+          <button class="header__formSubmit" type="submit"><i class="fa fa-search"></i></button>
+      </form>
+      <a href="/adminPage.html" class="header__adminPage">אדמין</a>
+  
+      <!-- user info / logout / edit user // show all posts -->
+  
+      <div class="header__userInfo">
+          <img onclick="showUserDropDown(event)"
+              src="/styles/img/menu.png">
+          <div class="header__userInfoDrop">
+              <a href="/Categories.html">קטגוריות</a>
+              <a href="/posts.html?myposts">פוסטים שלי</a>
+              <a href="/posts.html?myfavorites">מועדפים</a>
+              <a href="/updateUserData.html">עדכן פרטי חשבון</a>
+              <button onclick="handleLogout()">התנתק</button>
+          </div>
+      </div>`;
 
-    <!-- user info / logout / edit user // show all posts -->
-
-    <div class="header__userInfo">
-        <img onclick="showUserDropDown(event)"
-            src="https://icon-library.com/images/menu-icon-png-3-lines/menu-icon-png-3-lines-5.jpg">
-        <div class="header__userInfoDrop">
-            <a href="/Categories.html">קטגוריות</a>
-            <a href="/posts.html?myposts">פוסטים שלי</a>
-            <a href="/posts.html?myfavorites">מועדפים</a>
-            <a href="/updateUserData.html">עדכן פרטי חשבון</a>
-            <button onclick="handleLogout()">התנתק</button>
-        </div>
-    </div>`
-
-    let header = document.querySelector('.header')
-    header.innerHTML = html
-    getUserInfo(),
-        displayGoToAdminPage()
-}
-const getUserInfo = async  () => {
-    let user
-    await fetch("/userInfo")
-        .then((res) => res.json())
-        .then((data) => {
-            const name = data.decoded.fName;
-            sayHelloToUser(name);
-            user=data.decoded
-        });
-        return user;
+  let header = document.querySelector(".header");
+  header.innerHTML = html;
+  getUserInfo(), displayGoToAdminPage();
+};
+const getUserInfo = async () => {
+  let user;
+  await fetch("/userInfo")
+    .then((res) => res.json())
+    .then((data) => {
+      const name = data.decoded.fName;
+      sayHelloToUser(name);
+      user = data.decoded;
+    });
+  return user;
 };
 const sayHelloToUser = (name) => {
-    const myDate = new Date();
-    const hrs = myDate.getHours();
-    let greet;
-    let sayHello = document.querySelector('.header__helloUser')
+  const myDate = new Date();
+  const hrs = myDate.getHours();
+  let greet;
+  let sayHello = document.querySelector(".header__helloUser");
 
-    if (hrs < 12 && hrs>4)
-        greet = 'בוקר טוב';
-    else if (hrs >= 12 && hrs <= 16)
-        greet = 'צהרים טובים';
-    else if (hrs >= 16 && hrs <= 24)
-        greet = 'ערב טוב';
-    else if (hrs <= 4)
-        greet = 'לילה טוב';
-    sayHello.innerHTML = `${greet}, ${name}`
-}
+  if (hrs < 12 && hrs > 4) greet = "בוקר טוב";
+  else if (hrs >= 12 && hrs <= 16) greet = "צהרים טובים";
+  else if (hrs >= 16 && hrs <= 24) greet = "ערב טוב";
+  else if (hrs <= 4) greet = "לילה טוב";
+  sayHello.innerHTML = `${greet}, ${name}`;
+};
 
 const displayGoToAdminPage = async () => {
-    let checkAdmin = await handleCheckAdmin()
+  let checkAdmin = await handleCheckAdmin();
 
-    if (checkAdmin) {
-        document.querySelector('.header__adminPage').style.display = 'block'
-    }
-}
+  if (checkAdmin) {
+    document.querySelector(".header__adminPage").style.display = "block";
+  }
+};
 const showUserDropDown = (e) => {
-    document.querySelector('.header__userInfoDrop').style.display = 'flex'
-    e.stopPropagation();
-}
+  document.querySelector(".header__userInfoDrop").style.display = "flex";
+  e.stopPropagation();
+};
 const hideUserDropDown = () => {
-    document.querySelector('.header__userInfoDrop').style.display = 'none'
-}
-
-
+  document.querySelector(".header__userInfoDrop").style.display = "none";
+};
 
 //search bar
 const handleSearch = (e) => {
-    e.preventDefault()
-    const searched = document.querySelector('.header__formInput').value
-    if (searched.length > 2) {
-        let searchedSep = searched.replace(/\s/g, '-')
-        window.location.href = `/posts.html?search?${searchedSep}`
+  e.preventDefault();
+  const searched = document.querySelector(".header__formInput").value;
+  if (searched.length > 2) {
+    let searchedSep = searched.replace(/\s/g, "-");
+    window.location.href = `/posts.html?search?${searchedSep}`;
+  } else {
+    document.querySelector(".header__formInput").value = "";
+    document.querySelector(".header__formInput").placeholder =
+      "חיפוש חייב להיות מעל 2 תווים";
+  }
+};
 
-    } else {
-        document.querySelector('.header__formInput').value = ""
-        document.querySelector('.header__formInput').placeholder = "חיפוש חייב להיות מעל 2 תווים"
-    }
-}
-
-
-
-// hello user 
+// hello user
