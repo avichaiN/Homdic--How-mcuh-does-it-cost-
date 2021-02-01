cc65f84c55880302fb3d7b7a2d4fb25c13b2643f
Avichai Nissim <avichai74@gmail.com>
2021-02-01 13:15:57 +0200

    undo

--- public/js/headerNav.js
+++ public/js/headerNav.js
@@ -64,7 +64,6 @@ const displayGoToAdminPage = async () => {
 
   if (checkAdmin) {
     document.querySelector(".header__adminPage").style.visibility = "visible";
-    document.querySelector(".header").style.gridTemplateColumns = '0.4fr 0.15fr 1fr 0.2fr  0.15fr'
   }
 };
 const showUserDropDown = (e) => {
